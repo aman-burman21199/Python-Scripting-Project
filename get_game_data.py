d69f29d58f17a076e12ff5ba256e7e8944e80d6c
Aman Burman <amanb21199@gmail.com>
2023-12-29 23:37:41 +0530

    Added function to remove 'game' from path names

--- get_game_data.py
+++ get_game_data.py
@@ -12,20 +12,42 @@ def find_all_game_paths(source):
     game_paths = []
 
     for root, dirs, files in os.walk(source):
-        for directory in dirs:
+        for directory in dirs:  # dirs has names not full path
             if GAME_DIR_PATTERN in directory.lower():
                 path = os.path.join(source, directory)
                 game_paths.append(path)
-        break
+        break   # os.walk runs recursively for inner directories but we only want one level
 
     return game_paths
+
+def get_name_from_paths(paths, to_strip): # Remove "to_strip" from each path in paths
+    new_names = []
+    for path in paths:
+        # Split - Returns tuple "(head, tail)" where "tail" is everything after the final slash.
+        _ , dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip,"")
+        new_names.append(new_dir_name)
+
+    return new_names
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def copy():
+    pass
+
 
 def main(source, target):
     cwd = os.getcwd()   # Current Working Directory
     source_path = os.path.join(cwd,source)  # Don't do normal string concat as OS may differ
     target_path = os.path.join(cwd,target) 
 
+    game_paths = find_all_game_paths(source_path)
+    # print(game_paths)
+    new_game_dirs = get_name_from_paths(game_paths, "game")
 
+    create_dir(target_path)
 
 if __name__ == "__main__": 
     args = sys.argv # Get command line arguments
