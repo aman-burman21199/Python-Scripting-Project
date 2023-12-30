15e217d424b4c0dbdf08d749324b79b8a0037835
Aman Burman <amanb21199@gmail.com>
2023-12-30 14:55:30 +0530

    Added copy function and create metadata function

--- get_game_data.py
+++ get_game_data.py
@@ -34,9 +34,20 @@ def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
-def copy():
-    pass
+def copy_and_overwrite(source,dest):
+    # If dest exists, remove it
+    if os.path.exists(dest):
+        shutil.rmtree(dest) # recursive delete
 
+    shutil.copytree(source,dest)
+
+def make_json_metadata_file(path, game_dirs):
+    data = {
+        "gameNames" : game_dirs,
+        "numberOfGames" : len(game_dirs)
+    }
+    with open(path,"w") as f:
+        json.dump(data, f)
 
 def main(source, target):
     cwd = os.getcwd()   # Current Working Directory
@@ -46,8 +57,16 @@ def main(source, target):
     game_paths = find_all_game_paths(source_path)
     # print(game_paths)
     new_game_dirs = get_name_from_paths(game_paths, "game")
-
+    
     create_dir(target_path)
+
+    for src, dest in zip(game_paths, new_game_dirs):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
+
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path,new_game_dirs)
+
 
 if __name__ == "__main__": 
     args = sys.argv # Get command line arguments
